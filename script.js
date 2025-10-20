4dfc468364382e90ad0c8f054a772f26ac79e2a7
IvanMig89 <ivanmig89games@gmail.com>
2025-10-20 18:36:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,13 +10,32 @@ document.addEventListener("DOMContentLoaded", () => {
     "> CARICAMENTO DATI..."
   ];
 
+  const typingSound = new Audio("https://cdn.pixabay.com/audio/2022/03/15/audio_38d4f5b58f.mp3");
+  typingSound.volume = 0.2;
+
   let lineIndex = 0;
+
+  function typeLine(line, callback) {
+    let i = 0;
+    const interval = setInterval(() => {
+      bootText.textContent += line.charAt(i);
+      typingSound.currentTime = 0;
+      typingSound.play();
+      i++;
+      if (i >= line.length) {
+        clearInterval(interval);
+        bootText.textContent += "\n";
+        setTimeout(callback, 400);
+      }
+    }, 40);
+  }
 
   function showNextLine() {
     if (lineIndex < bootLines.length) {
-      bootText.textContent += bootLines[lineIndex] + "\n";
-      lineIndex++;
-      setTimeout(showNextLine, 1000);
+      typeLine(bootLines[lineIndex], () => {
+        lineIndex++;
+        showNextLine();
+      });
     } else {
       setTimeout(() => {
         bootScreen.classList.add("hidden");
